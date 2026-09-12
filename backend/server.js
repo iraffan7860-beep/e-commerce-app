46df03cb90bf201fcc464bff2cbf41bee30fc8a6
unknown <iraffan7860@gmail.com>
2026-09-12 18:52:53 +0530

    Fix CORS for localhost

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: "https://e-commerce-app-1-j7y9.onrender.com",
+  origin: ["http://localhost:5173", "https://e-commerce-app-1-j7y9.onrender.com"],
   credentials: true
 }));
 
